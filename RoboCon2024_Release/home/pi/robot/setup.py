7c0f47e80ca33ab4f89a14cabec4b45cfaaa58fc
IvyFailsCoding <126807273+IvyFailsCoding@users.noreply.github.com>
2024-01-13 14:40:35 +0000

    Adding RoboCon2024_Release patch to the repository

---
+++ RoboCon2024_Release/home/pi/robot/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="robot",
+    version="2024.1",
+    packages=["robot", "robot.marker_setup"],
+
+    install_requires=[
+    ],
+
+    author="Skyler Grey",
+    author_email="skyler3665@gmail.com",
+)
