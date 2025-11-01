4cc8b2651c351a8f1a72cb6f568c1b9881031916
Nathan Gill <nathan.j.gill@outlook.com>
2025-11-01 21:24:43 +0000

    add patches written at kickoff

---
+++ patches/RoboCon2026_p5/patch.sh
@@ -0,0 +1,18 @@
+PATCH_SUCCESS_ACTION="sudo reboot"
+DEBUG_LOG="/media/DebugUSB/brain_logfiles/debug_robocon_brain.txt"
+
+i2cset -y 1 8 25 1
+
+systemctl stop shepherd.service
+systemctl stop shepherd-resize_helper.service
+systemctl stop hopper.service
+systemctl stop shepherd-runner.service
+
+cp -av ${PATCH_DIRECTORY}/bin/hopper.server /bin/hopper.server                   >> ${DEBUG_LOG}
+
+systemctl enable hopper.service
+systemctl enable shepherd-runner.service
+systemctl enable shepherd.service
+systemctl enable shepherd-resize_helper.service
+
+i2cset -y 1 8 25 0
